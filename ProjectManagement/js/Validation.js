97e8aebcc340292ecfbbc89153255aac1a2c4f0c
jjspacer <thesantacruzian@gmail.com>
2014-04-10 17:17:50 -0700

    Date Validation was added to Validation.js
    I had to roll back the typescript

--- ProjectManagement/js/Validation.js
+++ ProjectManagement/js/Validation.js
@@ -9,6 +9,9 @@ function validatePopUp(object)
     		return 1;
 		}
 	}
+	var DateSuccess = DateValidation(object[2].value);
+	if (DateSuccess == 1)
+	    return 1;
 	pushValidatedTasks(object);
 	return 0;
 }
@@ -16,4 +19,12 @@ function pushValidatedTasks(object)
 {
 	var fb = new Firebase('https://burning-fire-7708.firebaseio.com/tasks');
 	fb.push({Title: object[0].value, Priority: object[4].value, To: object[3].value, By: object[1].value, FinishDate: object[2].value, Description: object[5].value});
-} 
+}
+function DateValidation(Day) {
+    var Today = new Date();
+    if (Today.toLocaleDateString() >= Day) {
+        alert("The date must be set to a day in the future.");
+        return 1;
+    }
+    return 0;
+}
