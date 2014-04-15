24928dc0db3ed3db608a966f5a85f9fab83c370f
jjspacer <thesantacruzian@gmail.com>
2014-04-15 10:28:55 -0700

    Initial Tester Branch
    this is for testing

--- ProjectManagement/js/Validation.js
+++ ProjectManagement/js/Validation.js
@@ -17,12 +17,26 @@ function validatePopUp(object)
 }
 function pushValidatedTasks(object)
 {
-	var fb = new Firebase('https://burning-fire-7708.firebaseio.com/tasks');
+	var fb = new Firebase('https://burning-fire-7708.firebaseio.com/test/tasks');
 	fb.push({Title: object[0].value, Priority: object[4].value, To: object[3].value, By: object[1].value, FinishDate: object[2].value, Description: object[5].value});
 }
 function DateValidation(Day) {
     var Today = new Date();
-    if (Today.toLocaleDateString() >= Day) {
+    var month, day;
+    if ((month = Today.getMonth() + 1) < 10) {
+        month = "0" + (Today.getMonth() + 1).toString();
+    }
+    else {
+        month = month.toString();
+    }
+    if ((day = Today.getDate()) < 10) {
+        day = "0" + Today.getDate.toString()
+    }
+    else {
+        day = day.toString();
+    }
+    var CompareValue = Today.getFullYear().toString() + "-" + month + "-" + day;
+    if (CompareValue >= Day) {
         DisplayAlertPopUp("Validation Error", "The date must be set to a day in the future.");
         return 1;
     }
