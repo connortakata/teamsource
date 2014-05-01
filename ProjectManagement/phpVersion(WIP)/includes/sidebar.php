eff13ab48b853dce7a9cf6c2199ce08259847895
dtfranz <doodondandee@gmail.com>
2014-05-01 14:30:28 -0700

    renamed calendar page, added sidebar logic

--- ProjectManagement/phpVersion(WIP)/includes/sidebar.php
+++ ProjectManagement/phpVersion(WIP)/includes/sidebar.php
@@ -1,10 +1,10 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
-			<ul class="nav nav-sidebar">
-				<li><a href="index.php">Dashboard</a></li>
-				<li class="active"><a href="calender.php">Calender</a></li>
-				<li><a href="resources.php">Resources</a></li>
-				<li><a href="tasks.php">Tasks</a></li>
-			</ul>
-    </div>
+            <ul class="nav nav-sidebar">
+                <li <?php if($_SERVER['PHP_SELF']=="/index.php") print 'class="active"';?>><a href="index.php">Dashboard</a></li>
+                <li <?php if($_SERVER['PHP_SELF']=="/calendar.php") print 'class="active"';?>><a href="calendar.php">Calendar</a></li>
+                <li <?php if($_SERVER['PHP_SELF']=="/resources.php") print 'class="active"';?>><a href="resources.php">Resources</a></li>
+                <li <?php if($_SERVER['PHP_SELF']=="/tasks.php") print 'class="active"';?>><a href="tasks.php">Tasks</a></li>
+            </ul>
+        </div>
