9e21c7ab4c28297db1b124058c1834031fdf483c
connortakata <takatac@spu.edu>
2014-05-19 22:32:24 -0700

    Double click tasks

--- ProjectManagement/phpVersion(WIP)/tasks.php
+++ ProjectManagement/phpVersion(WIP)/tasks.php
@@ -80,13 +80,13 @@
 
 		    	while($row = mysqli_fetch_array($result))
 				{
-						echo "<a href='#' class='list-group-item'>";
+						echo "<a href='#' class='list-group-item' ondblclick='EditPopup(" .  $row['ID'] . ")'>";
 			       		echo	"<h4 class='list-group-item-heading'>"; 
 					    echo	"<table width='100%'>";
 					    echo		"<td name='TaskTitle' style='width:200px;' size='15';><input type='checkbox'> " . $row['TASK_TITLE'] . "</td>";
-					    echo	    "<td style='width:200px;text-align:right' onclick='EditPopup(" .  $row['ID'] . ")' > Due: " . $row['TASK_DUE_DATE'] ."</td>";
-					    echo		"<td style='width:200px; text-align:center' onclick='EditPopup(" . $row['ID'] . ")' >To: " . $row['TASK_ASSIGNED_TO'] . "</td>";
-					    echo		"<td style='width:150px; text-align:right' onclick='EditPopup(" . $row['ID']  . ")' >Priority: " . $row['TASK_PRIORITY'] . "</td>";
+					    echo	    "<td style='width:200px;text-align:right'> Due: " . $row['TASK_DUE_DATE'] ."</td>";
+					    echo		"<td style='width:200px; text-align:center'> To: " . $row['TASK_ASSIGNED_TO'] . "</td>";
+					    echo		"<td style='width:150px; text-align:right'> Priority: " . $row['TASK_PRIORITY'] . "</td>";
 					    echo	"</table>";
 					    echo	"</h4>";
 					    echo "</a>";	
