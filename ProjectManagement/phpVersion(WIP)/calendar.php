eac8e2dae8869c4999a775b3c09d5c88c2b91a58
dtfranz <doodondandee@gmail.com>
2014-05-29 17:39:00 -0700

    Commented the calendar page's code

--- ProjectManagement/phpVersion(WIP)/calendar.php
+++ ProjectManagement/phpVersion(WIP)/calendar.php
@@ -41,8 +41,10 @@ print '
             </button>
         </div>';
 
+    //Print the calendar here
     printCalendar();
 
+    //These Scripts are specific to the Calendar page
     print '
     <script src="js/AJAX_lib.js"></script>
     <script>
@@ -81,9 +83,11 @@ print '
     		document.getElementById("btnSelEdit").onclick=function(){ AddEvent(true); HideSelectedPopup();};
     }
     </script>';
+
 function printCalendar()
 {
-    if(!isset($_GET["month"])||$_GET["month"]<1||$_GET["month"]>12)//check for $_POST data to see if another month or year was selected
+    //check for $_POST data to see if another month or year was selected
+    if(!isset($_GET["month"])||$_GET["month"]<1||$_GET["month"]>12)
         $month=date("n");
     else
         $month=$_GET["month"];
@@ -92,8 +96,8 @@ function printCalendar()
     else
         $year=$_GET["year"];
 
-    print '<ul style="display:table; margin:0 auto;" class="pagination">';
-    print '<li><a href="calendar.php?month='.($month).'&year='.($year-1).'">&laquo;</a></li>';
+    print '<ul style="display:table; margin:0 auto;" class="pagination">';//Print the month and year selector
+    print '<li><a href="calendar.php?month='.($month).'&year='.($year-1).'">&laquo;</a></li>';//Button actions depend on current month+year
     print '<li><a href="calendar.php?month='.($month).'&year='.$year.'">'.$year.'</a></li>';
     print '<li><a href="calendar.php?month='.($month).'&year='.($year+1).'">&raquo;</a></li></ul>';
     print '<ul style="display:table; margin:0 auto;" class="pagination">';
@@ -117,7 +121,7 @@ function printCalendar()
         print '<li><a href="calendar.php?month=1&year='.($year+1).'">&raquo;</a></li>';
     else
         print '<li><a href="calendar.php?month='.($month+1).'&year='.$year.'">&raquo;</a></li>';
-    print '</ul>'; //Start calendar table, print out the Top row with days of the week
+    print '</ul>'; //Start calendar table, print out the Top row with days of the week. This print should never change.
     print '<table style="margin: 0px auto;" class="calender">
                    <tr class="date">
                         <td class="days" width="11.43%">Sun</td>
@@ -132,13 +136,13 @@ function printCalendar()
     $dayNum = 0;
     $dayDetail = 0;
     if(isset($_GET["month"])&&isset($_GET["year"])&&$_GET["month"]>0&&$_GET["month"]<13&&$_GET["year"]>0&&$_GET["year"]<2040)
-        $days = buildCalArray($_GET["month"], $_GET["year"]);
+        $days = buildCalArray($_GET["month"], $_GET["year"]);//If we were given a month and/or year to work with, and they are valid, we use those
     elseif(isset($_GET["month"])&&$_GET["month"]>0&&$_GET["month"]<13)
-        $days = buildCalArray($_GET["month"]);
+        $days = buildCalArray($_GET["month"]);//If we were only given a month, we use that
     elseif(isset($_GET["year"])&&$_GET["year"]>0&&$_GET["year"]<2040)
-        $days = buildCalArray(NULL,$_GET["year"]);
+        $days = buildCalArray(NULL,$_GET["year"]);//If we were only given a year, we use that and the default value for month
     else
-        $days = buildCalArray();
+        $days = buildCalArray();//If we were given nothing, we make an array for the current year and month
 
     if($month<10)//Due to leading zeros in mysql, we need to deal with special cases (transitions from 09->10, 12->01, etc)
     {
@@ -164,6 +168,7 @@ function printCalendar()
         else
             $prevMonthYear = $year."-".($month-1);
     }
+
     //Select the team's calendar to identify with
     $teamID = $_SESSION["team"];
     $con = mysqli_connect("localhost", "root", "TeamSource1!", "teamsource");
@@ -173,6 +178,7 @@ function printCalendar()
     {
         $calID = $row[0];
     }
+
     //Find all events of the current month and adjacent months
     $sql = "SELECT ID, EVENT_TITLE, EVENT_DATETIME FROM EVENT
     WHERE (EVENT_DATETIME LIKE ('$monthYear%')
@@ -181,8 +187,9 @@ function printCalendar()
     AND EVENT_CALENDAR_ID='$calID'
     ORDER BY EVENT_DATETIME ASC";
     $result = mysqli_query($con,$sql);
-    $dayEvents = array(array(array()));
+    $dayEvents = array(array(array()));//Three dimensional array because we can have multiple events per day that each have their own array of info.
     $i=0;
+
     while($row = mysqli_fetch_array($result))//Generate an array of dates that can be referred to by a format like $dayEvents[5/31]
     {
         $date=substr($row["EVENT_DATETIME"],5,5);
@@ -193,7 +200,7 @@ function printCalendar()
             $day = substr($date,3);//shifts off the leading zero for day
             $date=substr($date,0,2).$day;
         }
-        $date=str_replace('-','/',$date);
+        $date=str_replace('-','/',$date);//Formatting the dates to form of 5/31
         if(!isset($dayEvents[$date]))
             $i=0;
         $dayEvents[$date][$i]["title"]=$row["EVENT_TITLE"];
@@ -207,7 +214,7 @@ function printCalendar()
         {
             print '<td class="days" width="11.43%"';
             if(($days[$dayNum][0]<$month)||($days[$dayNum][0]>$month))
-                print 'style="color:grey"';
+                print 'style="color:grey"';//Print grey color style for days not in the current month
             print '>';
             print substr($days[$dayNum],strpos($days[$dayNum],'/')+1);
             print '</td>';
@@ -220,9 +227,9 @@ function printCalendar()
             print '<td class="days" width="11.43%">';
             if(isset($dayEvents[$days[$dayDetail]]))
                 for($k=0;$k<count($dayEvents[$days[$dayDetail]]);$k++)
-                {
+                {//If statements here are to add some overflow behavior; maybe not necessary because of the css overflow property
                     if(strlen($dayEvents[$days[$dayDetail]][$k]["title"])>15)
-                    {
+                    {//Here, we generate a link that contains the necessary information to edit only the current event selected.
                         print '<a href="#" onclick="EditEvent('.$dayEvents[$days[$dayDetail]][$k]["ID"].');" title="'.$dayEvents[$days[$dayDetail]][$k]["title"].'">';
                         print substr($dayEvents[$days[$dayDetail]][$k]["title"],0,12)."...";
                     }
@@ -236,21 +243,21 @@ function printCalendar()
         }
         print '</tr>';
     }
-    print '</table></div>';
+    print '</table></div>';//Phew! All done...
 }
 
 function buildCalArray($month = NULL, $year=NULL)
 {
     if($month == NULL)
-        $month = date('n');
+        $month = date('n');//If we were sent no info for month, assume current month
     if($year == NULL)
-        $year =  date('Y');
-    $firstDay=date('w',strtotime($month."/1/".$year));
+        $year =  date('Y');//If we were sent no info for year, assume current year
+    $firstDay=date('w',strtotime($month."/1/".$year));//Gets the day of the week for the first day of the month
     $numDaysCurrMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year); //Get number of days in current month
-    if($month==1)
-        $numDaysPrevMonth = cal_days_in_month(CAL_GREGORIAN, 12, $year); //Number of days in last month
+    if($month==1)//Special case to get number of days in December of last year if the selected month is January
+        $numDaysPrevMonth = cal_days_in_month(CAL_GREGORIAN, 12, $year); //Number of days December of last year (Always same number of days in Dec, is this needed?)
     else
-        $numDaysPrevMonth = cal_days_in_month(CAL_GREGORIAN, $month-1, $year); //Number of days in last month
+        $numDaysPrevMonth = cal_days_in_month(CAL_GREGORIAN, $month-1, $year); //Otherwise, just number of days in last month
 
     $days = array();
     if($firstDay!=0)
@@ -266,7 +273,7 @@ function buildCalArray($month = NULL, $year=NULL)
     $day = 1;
     while($day<=$numDaysCurrMonth)
     {
-        array_push($days, $month."/".$day); //Inserts the days of the current month into the calendar
+        array_push($days, $month."/".$day); //Inserts the days of the current month into the calendar, from 1st to the last day
         $day++;
     }
     $day = 1;
@@ -275,7 +282,7 @@ function buildCalArray($month = NULL, $year=NULL)
         array_push($days, ($month+1)."/".$day); //Inserts the first days of the next month into the calendar
         $day++;
     }
-    return $days; //Finish with the built calendar
+    return $days; //Finish with the built calendar array
 }
+
 require "includes/footer.php";
-?>
