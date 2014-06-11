2f7d8e035ebaabe6f29b7f35e5a04f15878b3e8b
dtfranz <doodondandee@gmail.com>
2014-06-10 22:32:08 -0700

    removed extra files

--- ProjectManagement/phpVersion(WIP)2/AJAXapps/splash/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "../../includes/userAuth.php";
-if(isLoggedIn()){
-        unset($_SESSION["id"]);//Remove session data and kill the session to logout user.
-        unset($_SESSION["team"]);
-        session_destroy();
-}
